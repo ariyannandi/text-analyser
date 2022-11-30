c980cb4b1222ca257b6398f8c01428a192f0f3a4
ariyannandi <ariyannandi2711@gmail.com>
2022-11-30 22:48:38 +0530

    Updates

---
+++ precache-manifest.ba344db4b88d82fa47f24091a07cc405.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "58262434a4b8c1bd9529",
+    "url": "/text-analyser/static/css/main.839a850d.chunk.css"
+  },
+  {
+    "revision": "58262434a4b8c1bd9529",
+    "url": "/text-analyser/static/js/main.58262434.chunk.js"
+  },
+  {
+    "revision": "e45e0075b0567c8b67a9",
+    "url": "/text-analyser/static/js/1.e45e0075.chunk.js"
+  },
+  {
+    "revision": "1c4dfe7f8832301e9b7c",
+    "url": "/text-analyser/static/js/2.1c4dfe7f.chunk.js"
+  },
+  {
+    "revision": "547a7b2aef0520c6c663",
+    "url": "/text-analyser/static/js/runtime~main.547a7b2a.js"
+  },
+  {
+    "revision": "a693b11d1fa7b49b3d93d9664cea9b19",
+    "url": "/text-analyser/index.html"
+  }
+];
